6981fa8ebffd99669f400cc49bf19c5b468fdefe
Andrey Maksimov <maksimov.andrew@gmail.com>
2020-08-19 14:16:18 +0300

    Учебный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -6,15 +6,19 @@ const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-// Зафиксируем порт для сервера
 const DEFAULT_PORT = 8080;
 
 const app = express();
+const path = require(`path`);
 
-// Подключим созданные маршруты
+const PUBLIC_DIR = `public`;
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
-// Запуск сервера
 app.listen(DEFAULT_PORT);
